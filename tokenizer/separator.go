e9aa32dc3c3bdf0ad2bea61d55e045b2b3c7c0a0
Elias <p.elias.graf@gmail.com>
2021-05-24 20:49:04 +0200

    Fixed separator tokenizer

--- tokenizer/separator.go
+++ tokenizer/separator.go
@@ -5,8 +5,8 @@ var symbols = map[rune]bool{
 }
 
 func separator(inp string, idx int) (*Token, int, error) {
-	if len(inp) > idx && symbols[rune(inp[idx])] {
-		return &Token{Val: inp, Typ: Sep}, 1, nil
+	if symbols[rune(inp[idx])] {
+		return &Token{Val: string(inp[idx]), Typ: Sep}, 1, nil
 	}
 	return nil, 0, nil
 }
