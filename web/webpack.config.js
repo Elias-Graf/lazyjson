c728d296980fd4fccc96a5fde74296d3ad7fcf1f
Elias <p.elias.graf@gmail.com>
2021-08-09 15:36:34 +0200

    Added web

---
+++ web/webpack.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+    },
+    entry: "./index.ts",
+    experiments: { topLevelAwait: true },
+    module: {
+        rules: [{ test: /\.ts?$/, use: "ts-loader", exclude: /node_modules/ }],
+    },
+    output: { filename: "bundle.js", path: path.resolve(__dirname, "dist") },
+    plugins: [new HtmlWebpackPlugin()],
+    resolve: { extensions: [".tsx", ".ts", ".js"] },
+};
