14dd6c1b8fbb4903f45f73791d84b30e5fe58799
Elias <p.elias.graf@gmail.com>
2021-11-05 17:37:37 +0100

    Fixed error printing incorrectly

--- lazyjson/src/main.rs
+++ lazyjson/src/main.rs
@@ -1,19 +1,27 @@
 use std::env;
-use std::error::Error;
 use std::fs;
 
 use lazyjson;
 use lazyjson::treebuilder::config::Config;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), u8> {
     let args: Vec<String> = env::args().collect();
 
     let file_path = args.get(1).expect("no input file provided");
     let file = fs::read_to_string(file_path).expect("failed to read file");
 
-    let config = Config::from_iter(&mut args[2..].iter())?;
+    let config = match Config::from_iter(&mut args[2..].iter()) {
+        Err(e) => {
+            eprintln!("{}", e);
+            return Err(1);
+        }
+        Ok(c) => c,
+    };
 
-    lazyjson::parse(&file, &config)?;
+    if let Err(e) = lazyjson::parse(&file, &config) {
+        eprintln!("{}", e);
+        return Err(1);
+    }
 
     println!("successfully parsed");
 
